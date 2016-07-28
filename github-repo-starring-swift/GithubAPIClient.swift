aa210ac9a33e9d7fd4d16d2b648537c68e6e779f
jellybeanjohnny <mamerige@icloud.com>
2016-07-28 14:38:05 -0400

    completed lab to pass tests

--- github-repo-starring-swift/GithubAPIClient.swift
+++ github-repo-starring-swift/GithubAPIClient.swift
@@ -27,6 +27,77 @@ class GithubAPIClient {
         }
         task.resume()
     }
+  
+  class func checkIfRepositoryIsStarred(fullName: String, completion: Bool -> Void) {
+    let urlString = "\(githubAPIURL)/user/starred/\(fullName)?access_token=\(githubAccessToken)"
+    
+    guard let url = NSURL(string: urlString) else { return }
+    
+    let session = NSURLSession.sharedSession()
+    
+    let task = session.dataTaskWithURL(url) { (data, response, error) in
+      
+      let httpResponse = response as! NSHTTPURLResponse
+      
+      let isStarred: Bool = httpResponse.statusCode == 204
+      
+      completion(isStarred)
+      
+    }
+    task.resume()
+  }
+  
+  /**
+   PUT /user/starred/:owner/:repo
+   Note that you'll need to set Content-Length to zero when calling out to this endpoint.
+  */
+  class func starRepository(fullName: String, completion: ()->() ) {
+    guard let URL = NSURL(string:"\(githubAPIURL)/user/starred/\(fullName)") else { fatalError("Invalid URL") }
+    let request = NSMutableURLRequest(URL: URL)
+    request.HTTPMethod = "PUT"
+    request.addValue("token \(githubAccessToken)", forHTTPHeaderField: "Authorization")
+    request.addValue("0", forHTTPHeaderField: "Content-Length")
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithRequest(request) { (data, response, error) in
+      let httpResponse  = response as! NSHTTPURLResponse
+      if httpResponse.statusCode == 204 { print("Successfully starred repo: \(fullName)") }
+      completion()
+    }
+    task.resume()
+  }
+  
+  class func unstarRepository(fullName: String, completion: ()->() ) {
+    guard let URL = NSURL(string:"\(githubAPIURL)/user/starred/\(fullName)") else { fatalError("Invalid URL") }
+    let request = NSMutableURLRequest(URL: URL)
+    request.HTTPMethod = "DELETE"
+    request.addValue("token \(githubAccessToken)", forHTTPHeaderField: "Authorization")
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithRequest(request) { (data, response, error) in
+      let httpResponse  = response as! NSHTTPURLResponse
+      if httpResponse.statusCode == 204 { print("Successfully unstarred repo: \(fullName)") }
+      completion()
+    }
+    task.resume()
+  }
+  
+  class func toggleStarStatusForRepository(repository: String, completion: (Bool)->() ) {
+    // Check to see if this repository is starred
+    self.checkIfRepositoryIsStarred(repository) { (isStarred) in
+      if isStarred {
+        // Unstar the repo
+        self.unstarRepository(repository, completion: { 
+          completion(false)
+        })
+      }
+      else {
+        // star the repo
+        self.starRepository(repository, completion: { 
+          completion(true)
+        })
+      }
+    }
+  }
+  
     
 }
 
--- github-repo-starring-swift/ReposTableViewController.swift
+++ github-repo-starring-swift/ReposTableViewController.swift
@@ -9,36 +9,62 @@
 import UIKit
 
 class ReposTableViewController: UITableViewController {
+  
+  let store = ReposDataStore.sharedInstance
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    let store = ReposDataStore.sharedInstance
+    self.tableView.accessibilityLabel = "tableView"
+    self.tableView.accessibilityIdentifier = "tableView"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.tableView.accessibilityLabel = "tableView"
-        self.tableView.accessibilityIdentifier = "tableView"
-        
-        store.getRepositoriesWithCompletion {
-            NSOperationQueue.mainQueue().addOperationWithBlock({ 
-                self.tableView.reloadData()
-            })
+    store.getRepositoriesWithCompletion {
+      NSOperationQueue.mainQueue().addOperationWithBlock({
+        self.tableView.reloadData()
+      })
+    }
+  }
+  
+  
+  // MARK: - Table view data source
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.store.repositories.count
+  }
+  
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("repoCell", forIndexPath: indexPath)
+    
+    let repository:GithubRepository = self.store.repositories[indexPath.row]
+    cell.textLabel?.text = repository.fullName
+    
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    let cell = tableView.cellForRowAtIndexPath(indexPath)
+    
+    guard let repoName = cell?.textLabel?.text else { return }
+    
+    GithubAPIClient.toggleStarStatusForRepository(repoName) { (isStarred) in
+      
+      let alertController = UIAlertController(title: "", message: "", preferredStyle: .Alert)
+      let okayAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+      alertController.addAction(okayAction)
+     
+      dispatch_async(dispatch_get_main_queue(), { 
+        if isStarred {
+          alertController.title = "You just starred \(repoName)"
+          alertController.accessibilityLabel = "You just starred \(repoName)"
         }
+        else {
+          alertController.title = "You just unstarred \(repoName)"
+          alertController.accessibilityLabel = "You just unstarred \(repoName)"
+        }
+        self.presentViewController(alertController, animated: true, completion: nil)
+      })
     }
-
-    // MARK: - Table view data source
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.store.repositories.count
-    }
-
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("repoCell", forIndexPath: indexPath)
-
-        let repository:GithubRepository = self.store.repositories[indexPath.row]
-        cell.textLabel?.text = repository.fullName
-
-        return cell
-    }
-
+  }
+  
 }
